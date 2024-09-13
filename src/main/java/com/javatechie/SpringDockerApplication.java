0ae807bb918046a14f3399af1547d7538dc34092
kkirangit <kkiran1633@gmail.com>
2024-09-13 20:33:49 +0530

    first push

---
+++ src/main/java/com/javatechie/SpringDockerApplication.java
@@ -0,0 +1,22 @@
+package com.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringDockerApplication {
+
+	@GetMapping("/greeting")
+	public String greeting(){
+		return "Welcome to Javatechie Devops Course. Your App is running.";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerApplication.class, args);
+	}
+
+}
